d67038648baedf34b231e295495a6e9041fdc9d7
Anshul Soni <anshulsoni8109@gmail.com>
2025-04-27 18:19:57 +0530

    online server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const db = require('./db');
 
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 const person = require('./models/person');
 const MenuItem = require('./models/MenuItem');
@@ -19,7 +23,7 @@ const MenuItem = require('./models/MenuItem');
 const personRoutes = require('./routes/personRoutes');
 app.use('/menu',personRoutes);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log(`listening port is running`)
     
 })
